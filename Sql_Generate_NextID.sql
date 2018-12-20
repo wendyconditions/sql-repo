b51009776692adadb0322a880ef2d3a7b68cae02
Wendy Maldonado <wendymaldonado.la@gmail.com>
2018-12-20 12:23:41 -0800

    Refactor, using scalar valued function

--- Sql_Generate_NextID.sql
+++ Sql_Generate_NextID.sql
@@ -6,27 +6,21 @@
 -- Parameters:	@CustomerID: This will determine what the next ID available is for that customer.
 -- ReturnValue: String TitleID - customer title count plus 1
 -- =============================================
-ALTER FUNCTION [dbo].[AR_udf_GetNextTitleIDByCustomer](@CustomerID int)
-
-RETURNS @Final TABLE(TitleID nvarchar(20))
+ALTER FUNCTION [dbo].[AR_udf_NextTileIDbyCustomerID](
+                @customerID int)
+RETURNS nvarchar(20)
 AS
 BEGIN
-		If (select count(TitleID) from TableA WHERE CustomerID = @CustomerID) = 0
-		BEGIN
-			INSERT INTO @Final
-				SELECT c.Abbr + '-' + '1'
-				FROM TableB
-				WHERE c.customerid = @CustomerID
-		END
-		ELSE
-		BEGIN
-			INSERT INTO @Final
-				SELECT c.Abbr + '-' + CAST(COUNT(t.TitleID) + 1 AS NVARCHAR(20))
-				FROM TableB c join
-				TableA as t
-				ON t.CustomerID = c.CustomerID
-				WHERE c.customerid = @CustomerID
-				GROUP BY c.Abbr
-		END
-		RETURN;
+    DECLARE @titleID nvarchar(20);
+
+    SELECT @titleID = c.Abbr+'-'+CAST(
+                                      (
+                                       SELECT COUNT(titleID)
+                                         FROM AR_Title
+                                         WHERE CustomerID = @customerID
+                                      ) + 1 AS nvarchar(20))
+      FROM drms.dbo.Customer c
+      WHERE c.CustomerID = @customerID;
+
+    RETURN @titleID;
 END;
